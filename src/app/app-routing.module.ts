e19cf4e92908fbdbcb3a69d301d8a853e32c2fee
u4bi <rootcode10@gmail.com>
2017-10-20 07:41:27 +0900

    home and test component router render

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'test',
+    component: TestComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)    
   ],
-  declarations: []
+  declarations: [HomeComponent, TestComponent],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
